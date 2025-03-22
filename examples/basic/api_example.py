1cfc5026cc0851166ab839995ce86e3c00d8b9e8
crayon <873217631@qq.com>
2025-03-23 00:53:34 +0800

    chore: update code quality workflow and API example # Reviewed, transaction id: 35504

--- examples/basic/api_example.py
+++ examples/basic/api_example.py
@@ -1,6 +1,6 @@
 from throttled import Throttled
 
-# 参数全部缺省时，默认初始化一个基于「内存」、每秒允许通过 60 个请求、使用「令牌桶算法」的限流器。
+# 参数全部缺省时，默认初始化一个基于「内存」、每分钟允许通过 60 个请求、使用「令牌桶算法」的限流器。
 throttle = Throttled()
 
 # 消耗 1 次请求，输出：RateLimitResult(limited=False,
@@ -10,5 +10,5 @@ print(throttle.limit("key", 1))
 print(throttle.peek("key"))
 
 # 消耗 60 次请求，触发限流，输出：RateLimitResult(limited=True,
-# # state=RateLimitState(limit=60, remaining=59, reset_after=1))
+# state=RateLimitState(limit=60, remaining=59, reset_after=1))
 print(throttle.limit("key", 60))
